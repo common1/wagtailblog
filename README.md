1406f526cd6362e8f638d10236331a874b2ad199
Harrie Smulders <h.l.w.smulders@tue.nl>
2025-11-26 16:47:14 +0100

    551. Mastering Wagtail CMS Page Context

--- README.md
+++ README.md
@@ -52,3 +52,8 @@ python manage.py startapp documents
 ```bash
 ```
 
+## 551. Mastering Wagtail CMS Page Context
+
+```bash
+```
+
--- blogpages/models.py
+++ blogpages/models.py
@@ -20,11 +20,17 @@ class BlogIndex(Page):
         FieldPanel('body'),
     ]
 
+    def get_context(self, request):
+        context = super().get_context(request)
+        context['blogpages'] = BlogDetail.objects.live().public()
+        return context
+
 class BlogDetail(Page):   
-    subtitle = models.CharField(max_length=100, blank=True)
-    body = RichTextField(blank=True)
     parent_page_types = ['blogpages.BlogIndex']
     subpage_types = []
+
+    subtitle = models.CharField(max_length=100, blank=True)
+    body = RichTextField(blank=True)
     
     content_panels = Page.content_panels + [
         FieldPanel('subtitle'),
